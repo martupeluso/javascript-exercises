4d7582fae3177b28c072a1a2726cb7aecf2b112f
Martina Peluso <185727137+martupeluso@users.noreply.github.com>
2025-03-09 18:19:08 +0000

    feat: add solution to exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // true if year is divisible by 4 but not by 100
+    // OR if year is divisible by 400
+    return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+ 
